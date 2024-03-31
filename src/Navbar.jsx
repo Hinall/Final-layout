b99cedb5a63eb5f9e4927e474900142cd4806adc
hinal <102239391+Hinall@users.noreply.github.com>
2024-03-31 15:52:28 +0530

    button added

---
+++ src/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+const Navbar = () => {
+  return (
+     <Button variant="primary">Primary Button</Button>
+  )
+}
+
+export default Navbar
