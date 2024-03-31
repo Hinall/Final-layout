b348e1510b4cb6d99c5ba466c1e48f0577529aec
hinal <102239391+Hinall@users.noreply.github.com>
2024-03-31 16:14:10 +0530

    second button

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 const Navbar = () => {
-  return (
-     <Button variant="primary">Primary Button</Button>
+  return (<>
+  <Button variant="primary">Primary Button</Button>
+     <Button variant="secondary">Primary Button</Button>
+  </>
+     
   )
 }
 
