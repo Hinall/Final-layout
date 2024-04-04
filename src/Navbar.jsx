9e5078a3d1a1790eae9949d83cf517dfa18a244f
hinal <102239391+Hinall@users.noreply.github.com>
2024-04-04 22:44:18 +0530

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
-const Navbar = () => {
+import  { Button,Container,Nav,NavDropdown,NavItem,Navbar, NavbarBrand, NavbarOffcanvas, NavbarToggle, Offcanvas } from 'react-bootstrap';
+const MyNavbar = () => {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleOffcanvasToggle = () => setShowOffcanvas(!showOffcanvas);
+
   return (<>
-  <Button variant="primary">Primary Button</Button>
-     <Button variant="secondary">Primary Button</Button>
-  </>
-     
+ <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleOffcanvasToggle} />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Offcanvas show={showOffcanvas} onHide={handleOffcanvasToggle}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Menu</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <Nav className="flex-column">
+            <Nav.Link href="#home" onClick={handleOffcanvasToggle}>Home</Nav.Link>
+            <Nav.Link href="#link" onClick={handleOffcanvasToggle}>Link</Nav.Link>
+            <Nav.Link href="#about" onClick={handleOffcanvasToggle}>About</Nav.Link>
+          </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
+    
+  
+  
+</>
   )
 }
 
-export default Navbar
+export default MyNavbar
